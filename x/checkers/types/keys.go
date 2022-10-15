63d68a90a649e0e6b436fe5c7a07d6a80c3af34d
Leonardo Digiorgio <simon.digiorgio@gmail.com>
2022-10-15 11:24:37 +0200

    Record Winner and expiring games

--- x/checkers/types/keys.go
+++ x/checkers/types/keys.go
@@ -42,6 +42,7 @@ const (
 	MovePlayedEventCapturedX = "captured-x"
 	MovePlayedEventCapturedY = "captured-y"
 	MovePlayedEventWinner    = "winner"
+	MovePlayedEventBoard     = "board"
 )
 
 const (
@@ -55,6 +56,13 @@ const (
 )
 
 const (
-	MaxTurnDuration = time.Duration(24 * 3_600 * 1000_000_000) // 1 day
+	MaxTurnDuration = time.Duration(1 * 60 * 1000_000_000) // 1 minutes
 	DeadlineLayout  = "2006-01-02 15:04:05.999999999 +0000 UTC"
+)
+
+const (
+	GameForfeitedEventType      = "game-forfeited"
+	GameForfeitedEventGameIndex = "game-index"
+	GameForfeitedEventWinner    = "winner"
+	GameForfeitedEventBoard     = "board"
 )
